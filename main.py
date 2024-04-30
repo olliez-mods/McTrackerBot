a47084d5b107bf7e90a6d6149c526dce3c323c73
olliez-mods <socksinthewash@gmail.com>
2024-04-30 18:43:10 -0400

    Create config.ini, and read in values into main.py

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import sqlite3
 import threading
 import time
+import configparser
 from SQL_functions import *
 
+# Read in config file
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Assign config to static variables
+USE_BOT: bool = (config['Settings']['use_bot'] == "1")
+BOT_TOKEN: str = config['Settings']['bot_token']
+SQL_DATABASE: str = config['Settings']['SQL_database']
 
 # Create SQL connection and curser
-SQL_connection = sqlite3.connect("McDiscBot.db")
+SQL_connection = sqlite3.connect(SQL_DATABASE)
 SQL_cursor = SQL_connection.cursor()
 
 
 # Make sure the tables exist that we want
-if(not table_exists(SQL_cursor, "disc_servers")):
+if(USE_BOT and not table_exists(SQL_cursor, "disc_servers")):
     print("Creating \"disc_servers\" SQL table")
     SQL_cursor.execute('''CREATE TABLE disc_servers (
                        server_id INT PRIMARY KEY,
