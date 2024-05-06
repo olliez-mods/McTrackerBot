dcc97fd7888d20151b137b1cdb4e36740f8880ff
olliez-mods <socksinthewash@gmail.com>
2024-05-06 16:56:18 -0400

    Moved server status into threads

--- minecraft_player_logger.py
+++ minecraft_player_logger.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from SQL_functions import *
 from get_mc_server_status import *
 import uuid
+import threading
 
 
 def get_uuid() -> str:
@@ -12,6 +13,39 @@ def get_uuid() -> str:
 def add_record(connection: sqlite3.Connection, cursor: sqlite3.Cursor, uuid: str, timestamp: str, s_reboot: int, connected: int, online: int, player_count: int, players: list[str]):
     cursor.execute(f"INSERT INTO LOGS_{uuid} (timestamp, s_reboot, connected, online, player_count, players) VALUES ('{timestamp}', {s_reboot}, {connected}, {online}, {player_count}, '{','.join(players)}')")
     connection.commit()
+
+def update_server_logs_thread(database: str, uuid:str, ip:str,  port:int):
+
+    # Create SQL connection and curser
+    connection = sqlite3.connect(database)
+    cursor = connection.cursor()
+
+    if(not table_exists(cursor, f"LOGS_{uuid}")):
+        print("New unlogged server... creating TABLE")
+        create_logs_table(cursor, uuid)
+
+    # Returns None if the row isn't found
+    cursor.execute(f"SELECT * FROM LOGS_{uuid} ORDER BY timestamp DESC LIMIT 1")
+
+    # Fetch the result (the row with the most recent datetime value)
+    last_log = cursor.fetchone()
+
+    status = get_status(ip, port)
+    if('players' in status): status['players'].sort()
+
+    make_log = False
+    if(last_log == None): make_log = True
+    elif(status['connected'] != last_log[2]): make_log = True
+    elif(status['online'] != last_log[3]): make_log = True
+    elif(status['count'] != last_log[4]): make_log = True
+    elif(len(last_log) >= 6 and not all(x == y for x, y in zip(status['players'], last_log[5].split(",")))): make_log = True
+
+    if(make_log):
+        timestamp = str(datetime.now())
+        add_record(connection, cursor, uuid, timestamp, 0, status['connected'], status['online'], status['count'], status['players'])
+
+    connection.close()
+
 
 def start(sql_database: str, timeout: int=10):
 
@@ -23,8 +57,6 @@ def start(sql_database: str, timeout: int=10):
     SQL_connection = sqlite3.connect(sql_database)
     SQL_cursor = SQL_connection.cursor()
 
-    timestamp = str(datetime.now())
-
     while(True):
         iteration_start = time.time()
 
@@ -32,39 +64,14 @@ def start(sql_database: str, timeout: int=10):
         SQL_cursor.execute('SELECT * FROM mc_servers')
         rows: list[any] = SQL_cursor.fetchall()
         
-        print("start")
-        t_stamp = time.time()
         #Loop through each server
         for mc_server in rows:
             ip: str = mc_server[0]
             port: int = mc_server[1]
             uuid: str = mc_server[2]
 
-            if(not table_exists(SQL_cursor, f"LOGS_{uuid}")):
-                print("New unlogged server... creating TABLE")
-                create_logs_table(SQL_cursor, uuid)
-
-            # Returns None if the row isn't found
-            SQL_cursor.execute(f"SELECT * FROM LOGS_{uuid} ORDER BY timestamp DESC LIMIT 1")
-
-            # Fetch the result (the row with the most recent datetime value)
-            last_log = SQL_cursor.fetchone()
-
-            status = get_status(ip, port)
-            if('players' in status): status['players'].sort()
-
-            make_log = False
-            if(last_log == None): make_log = True
-            elif(status['connected'] != last_log[2]): make_log = True
-            elif(status['online'] != last_log[3]): make_log = True
-            elif(status['count'] != last_log[4]): make_log = True
-            elif(len(last_log) >= 6 and not all(x == y for x, y in zip(status['players'], last_log[5].split(",")))): make_log = True
-
-            if(make_log):
-                timestamp = str(datetime.now())
-                add_record(SQL_connection, SQL_cursor, uuid, timestamp, 0, status['connected'], status['online'], status['count'], status['players'])
-
-        print("ended with:", time.time() - t_stamp)
+            t = threading.Thread(target=update_server_logs_thread, args=(sql_database, uuid, ip, port), daemon=True)
+            t.start()
 
         iteration_end = time.time()
 
