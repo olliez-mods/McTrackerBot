b243ecf1ab6ff379b044e96e955fd405984bb459
olliez-mods <socksinthewash@gmail.com>
2024-05-07 23:57:45 -0400

    Let user choose to wait for connection before fully booting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sqlite3
 import threading
 import time
+import requests
 import os
 import configparser
 from SQL_functions import *
@@ -28,6 +29,11 @@ SQL_database=bot_database.db
 status_update_timeout=3
 # Timeout between updating pinned messages (May be rate limited if beloew 10)
 pinned_message_timeout=10
+                
+# Wait for internet connection before attempting to start. (If set to 0, we will quit immediately)
+wait_for_connection_on_boot=0
+# How many seconds to wait before just giving up and quitting (set to -1 no never quit!)
+time_before_fail=180
 """)
     exit()
 
@@ -42,6 +48,31 @@ OWNER_ID: int = int(config['Settings']['owner_id'])
 SQL_DATABASE: str = config['Settings']['SQL_database']
 STATUS_TIMEOUT: int = int(config['Settings']["status_update_timeout"])
 PINNED_TIMEOUT: int = int(config['Settings']["pinned_message_timeout"])
+WAIT_FOR_CONNECTION: bool = (config['Settings']['wait_for_connection_on_boot'] == "1")
+TIME_BEFORE_FAIL:int = int(config['Settings']['time_before_fail'])
+
+
+def check_internet_connection():
+    try:
+        response = requests.get("http://www.google.com", timeout=5)
+        return response.status_code == 200
+    except requests.ConnectionError:
+        return False
+if(not check_internet_connection()):
+    if(not WAIT_FOR_CONNECTION):
+        print("Internet connection not found :(. Goodbye\n")
+        exit()
+    print(f"Internet connection not found (Timeout={TIME_BEFORE_FAIL}):")
+    print(" - Waiting...  ", end="", flush=True)
+    t_start = time.time()
+    while(not check_internet_connection()):
+        if(TIME_BEFORE_FAIL > -1 and time.time() - t_start > TIME_BEFORE_FAIL):
+            print("\n - Couldn't connect to the internet in time :(. Goodbye\n")
+            exit()
+        time.sleep(1)
+        print(f"\r - Waiting... {int(time.time() - t_start)}", flush=True, end="")
+    print("\n - Internet connection established :D\n")
+
 
 # Create SQL connection and curser
 SQL_connection = sqlite3.connect(SQL_DATABASE)
